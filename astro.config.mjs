9f0a297ef77cc024bd713b663da46306efa7d2d2
Rid <alridho2003@gmail.com>
2024-10-04 22:11:33 +0800

    updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  site: "https://rid333.github.io",
+  base: "/personal-blog",
 });
+
